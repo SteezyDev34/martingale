57acfe85634d04e52f411914c80ff3a82ebc0538
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-11 16:34:02 +0200

    fix getbet on windows vps

--- Functions/GetBet.py
+++ Functions/GetBet.py
@@ -46,7 +46,7 @@ def GetBet(driver, nextBet=False):
         if config.scriptType == '300' or config.scriptType == '030':
             sautDeLigne = 50 * 3
         else:
-            sautDeLigne = 62
+            sautDeLigne = 70
         decalageX = -50
 
     ligne = 1
