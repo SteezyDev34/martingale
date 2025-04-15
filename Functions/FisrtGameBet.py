da5c6443b13bc5d543242448887f85ac97d4432c
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-15 09:27:49 +0200

    improve result

--- Functions/FisrtGameBet.py
+++ Functions/FisrtGameBet.py
@@ -44,6 +44,7 @@ def FirstGameBet(driver):
         if not GetBet(driver, nextBet):
             tentative = tentative + 1
             if tentative > 5:
+                config.error = True
                 config.log('error recup jeu #ERR345', 'error', True, 2)
                 current_frame = inspect.currentframe()
                 config.log(
