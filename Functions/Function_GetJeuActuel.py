5ec165d08eb3b8434c3199f3c38a9b73834b5351
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-15 09:54:21 +0200

    improve result

--- Functions/Function_GetJeuActuel.py
+++ Functions/Function_GetJeuActuel.py
@@ -1,67 +1,82 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-import Functions.Function_GetSetActuel
-from Functions.Function_GetSetActuel import GetSetActuel
 import config
-from selenium.webdriver.support import expected_conditions as EC
+from Functions.Function_GetSetActuel import GetSetActuel
+from Functions.GetIfMatchPage import GetIfMatchPage
 
 
 def GetJeuActuel(driver):
-    config.jeu_actuel = 0
-    try:
-        WebDriverWait(driver, 20).until(
-            EC.visibility_of_element_located((By.CLASS_NAME,
-                                              'scoreboard-periods-table__col'))
-        )
-        config.jeu_actuel = driver.find_elements(By.CLASS_NAME, 'scoreboard-periods-table__col')
-    except Exception as e:
-        config.log(f"#E0009\nUne erreur est survenue : {e}")
-        config.log("erreur : c-scoreboard-player-score__row")
-        return False
-    else:
+    config.jeu_actuel = False
+    tentative = 0
+    while not config.jeu_actuel:
         try:
-            # print('get set')
-            GetSetActuel(driver)
-            if config.set_actuel == '1':
-                jeu_actuel_player1 = \
-                config.jeu_actuel[1].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
-                jeu_actuel_player2 = \
-                config.jeu_actuel[1].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
-                config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
-            elif config.set_actuel == '2':
-                jeu_actuel_player1 = \
-                config.jeu_actuel[2].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
-                jeu_actuel_player2 = \
-                config.jeu_actuel[2].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
-                config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
-            elif config.set_actuel == '3':
-                jeu_actuel_player1 = \
-                config.jeu_actuel[3].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
-                jeu_actuel_player2 = \
-                config.jeu_actuel[3].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
-                config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
-            elif config.set_actuel == '4':
-                jeu_actuel_player1 = \
-                config.jeu_actuel[4].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
-                jeu_actuel_player2 = \
-                config.jeu_actuel[4].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
-                config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
-            elif config.set_actuel == '5':
-                jeu_actuel_player1 = \
-                config.jeu_actuel[5].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
-                jeu_actuel_player2 = \
-                config.jeu_actuel[5].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
-                config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
-            else:
-                return False
+            WebDriverWait(driver, 20).until(
+                EC.visibility_of_element_located((By.CLASS_NAME,
+                                                  'scoreboard-periods-table__col'))
+            )
+            config.jeu_actuel = driver.find_elements(By.CLASS_NAME, 'scoreboard-periods-table__col')
         except Exception as e:
-            config.log(f"#JEU0010\nUne erreur est survenue : {e}")
-            config.log("erreur : numjeu")
-            return False
+            config.log(f"#E0009\nUne erreur est survenue : {e}")
+            config.log("erreur : c-scoreboard-player-score__row")
+            tentative = tentative + 1
+            if not GetIfMatchPage(driver):
+                config.error = True
+            else:
+                tentative = tentative + 1
+                if tentative == 5:
+                    config.error = True
         else:
-            # config.log('Récupération du jeu actuel : ' + str(config.jeu_actuel))
-            return True
+            try:
+                # print('get set')
+                GetSetActuel(driver)
+                if config.set_actuel == '1':
+                    jeu_actuel_player1 = \
+                        config.jeu_actuel[1].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
+                    jeu_actuel_player2 = \
+                        config.jeu_actuel[1].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
+                    config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
+                elif config.set_actuel == '2':
+                    jeu_actuel_player1 = \
+                        config.jeu_actuel[2].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
+                    jeu_actuel_player2 = \
+                        config.jeu_actuel[2].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
+                    config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
+                elif config.set_actuel == '3':
+                    jeu_actuel_player1 = \
+                        config.jeu_actuel[3].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
+                    jeu_actuel_player2 = \
+                        config.jeu_actuel[3].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
+                    config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
+                elif config.set_actuel == '4':
+                    jeu_actuel_player1 = \
+                        config.jeu_actuel[4].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
+                    jeu_actuel_player2 = \
+                        config.jeu_actuel[4].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
+                    config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
+                elif config.set_actuel == '5':
+                    jeu_actuel_player1 = \
+                        config.jeu_actuel[5].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[0]
+                    jeu_actuel_player2 = \
+                        config.jeu_actuel[5].find_elements(By.CLASS_NAME, 'scoreboard-periods-table-cell--td')[1]
+                    config.jeu_actuel = int(jeu_actuel_player1.text) + int(jeu_actuel_player2.text) + 1
+                else:
+                    return False
+            except Exception as e:
+                config.log(f"#JEU0010\nUne erreur est survenue : {e}")
+                config.log("erreur : numjeu")
+                if not GetIfMatchPage(driver):
+                    config.error = True
+                else:
+                    tentative = tentative + 1
+                    if tentative == 5:
+                        config.error = True
+                return False
+            else:
+                # config.log('Récupération du jeu actuel : ' + str(config.jeu_actuel))
+                return True
+    return False
 
 
 if __name__ == "__main__":
