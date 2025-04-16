05aa41eb8f8030dd5416eca33faad9d82092c43c
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-16 18:56:30 +0200

    improve result

--- Functions/Functions_40a_proba.py
+++ Functions/Functions_40a_proba.py
@@ -22,7 +22,7 @@ from Functions.retour_section_tps_reglementaire import RetourTpsReg
 
 def all_script(driver):
     driver.switch_to.window(driver.window_handles[0])
-    lose = True
+    result = False
     # Mise à jour du fichier txt des script en cours
     #
     scriptDelRunning()
