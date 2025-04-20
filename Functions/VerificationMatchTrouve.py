76d73c6b36f7bd853f72788f8c9a1762534e6c82
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-20 13:47:34 +0200

    fix score array

--- Functions/VerificationMatchTrouve.py
+++ Functions/VerificationMatchTrouve.py
@@ -24,7 +24,6 @@ def main(driver, bet_item, matchlist_file_name):
         return [False, config.newmatch]
     else:
         match_list = GetMatchDone.main(config.matchlisttodo_file_name)
-        print(match_list)
         print(config.matchlisttodo_file_name)
         if any(config.newmatch in x for x in match_list):
             config.log('Le match autorisé!', 'success', False, 4)
