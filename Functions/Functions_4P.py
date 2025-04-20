9d222de78c8913fffa547f4e96af089e1b968cfb
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-20 16:12:24 +0200

    fix score array

--- Functions/Functions_4P.py
+++ Functions/Functions_4P.py
@@ -231,7 +231,7 @@ def all_script(driver):
                     SendGlobalPerte(config.scriptType, m)
                     config.perte = float(infosperte['perte'])
             config.log(f'Net profit: {config.global_match_win}')
-            if config.perte == 0 and config.global_match_win < 1:
+            if float(config.perte) == 0 and float(config.global_match_win) < 1:
                 continue
             elif config.nb_tour <= winmatch:
                 break
--- Functions/Functions_5P.py
+++ Functions/Functions_5P.py
@@ -248,7 +248,7 @@ def all_script(driver):
                     config.perte = float(infosperte['perte'])
                 config.rattrape_perte = 1
             config.log(f'Net profit: {config.global_match_win}')
-            if config.perte == 0 and config.global_match_win < 1:
+            if float(config.perte) == 0 and float(config.global_match_win) < 1:
                 continue
             elif config.nb_tour <= winmatch:
                 break
