49d31648802b0a367323012ed6104ba7b34dc666
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-22 12:40:12 +0200

    fix score array

--- Functions/Authenticator.py
+++ Functions/Authenticator.py
@@ -11,10 +11,13 @@ def get_authenticator_code(driver):
     Opens the authenticator extension and retrieves the code from the HTML element
     """
     extension_id = "bhghoamapcdpbohphigoooaddinpkbai"
+    original_tab = driver.current_window_handle   # 1) onglet courant
+
+    driver.switch_to.new_window("tab")
+
     driver.get(f"chrome-extension://{extension_id}/view/popup.html")
 
     # Wait for the codes element to be present and get the code
-
     try:
         # Wait for the code element with timeout class to be visible
         WebDriverWait(driver, 30).until(
@@ -29,6 +32,11 @@ def get_authenticator_code(driver):
         # Get the code element and its value after timeout disappeared
         code_element = driver.find_element(By.CLASS_NAME, "code")
         auth_code = code_element.text
+        # 4) Ferme l’onglet courant
+        driver.close()
+
+        # 5) Reviens à l’onglet d’origine
+        driver.switch_to.window(original_tab)
         return auth_code
     except Exception as e:
         print(f"Error getting authentication code: {e}")
@@ -75,15 +83,22 @@ def loginProcess(driver, username="7696755", password="Scorpio971n#1xbet3"):
     """
     Logs into the account using provided credentials
     """
+    # Setup Chrome driver
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    driver = webdriver.Chrome(options=options)
     loginsuccessful = False
     while not loginsuccessful:
         try:
-            # Get authentication code
-            print("Getting 2FA code from authenticator...")
             auth_code = get_authenticator_code(driver)
-
             # Navigate to Lollybet
-            driver.get("https://ca.1xbet.com/fr/user/login")
+            driver.get("https://ca.1xbet.com/fr/")
+
+            auth_dropdown = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-component="AuthDropdown"]'))
+            )
+            auth_dropdown.click()
+
             # Wait for form fields to be present
             print("Waiting for login form fields...")
             authFields = WebDriverWait(driver, 10).until(
@@ -130,6 +145,9 @@ def loginProcess(driver, username="7696755", password="Scorpio971n#1xbet3"):
                 EC.presence_of_element_located((By.CLASS_NAME, "auth-form-two-step"))
             )
 
+            # Get authentication code
+            print("Getting 2FA code from authenticator...")
+            
             # Find and fill 2FA code input field
             print("Entering 2FA code...")
             code_field = driver.find_element(By.CSS_SELECTOR, ".auth-form-two-step__field input")
