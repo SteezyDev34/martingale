1aa50496d62083b0cf673a759fe8b38f8c82c213
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-22 23:50:55 +0200

    fix score array

--- Functions/Functions_15a.py
+++ Functions/Functions_15a.py
@@ -44,11 +44,11 @@ def all_script(driver):
         infosperte = getGlobalPerte()
         print("PERTE : ")
         if infosperte and config.perte == 0:
-            if float(infosperte['perte']) > 10:
-                SendGlobalPerte(config.scriptType, -10)
-                config.perte = 10
+            if float(infosperte['perte']) > 1:
+                SendGlobalPerte(config.scriptType, -1)
+                config.perte = 1
                 config.rattrape_perte = 1
-            elif float(infosperte['perte']) <= 10:
+            elif float(infosperte['perte']) <= 1:
                 config.perte = float(infosperte['perte'])
                 m = 0 - config.perte
                 SendGlobalPerte(config.scriptType, m)
@@ -194,11 +194,11 @@ def all_script(driver):
             infosperte = getGlobalPerte()
             print("PERTE : ")
             if infosperte:
-                if float(infosperte['perte']) > 10:
-                    SendGlobalPerte(config.scriptType, -10)
-                    config.perte = 10
+                if float(infosperte['perte']) > 1:
+                    SendGlobalPerte(config.scriptType, -1)
+                    config.perte = 1
                     config.rattrape_perte = 1
-                elif float(infosperte['perte']) <= 10:
+                elif float(infosperte['perte']) <= 1:
                     config.perte = float(infosperte['perte'])
                     m = 0 - config.perte
                     SendGlobalPerte(config.scriptType, m)
