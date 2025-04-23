6c3caed4b4689dfe72a4c6307e75117943536272
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-23 20:55:44 +0200

    improve looking game

--- Functions/AfficherParis.py
+++ Functions/AfficherParis.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
