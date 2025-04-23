cb9988450e532b4f7ee5b0617576b382263e135c
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-24 00:34:48 +0200

    improve looking game

--- Functions/GetBet.py
+++ Functions/GetBet.py
@@ -75,31 +75,31 @@ def GetBet(driver, nextBet=False):
             if (len(first_player) > 0 and not nextBet) or (len(first_player) == 0 and nextBet):
                 first_player = 1
                 if config.scriptType == '4030':
-                    config.win_type = '30:40'  # inversé
+                    config.win_type = '40:30'
                     win_texte = '40-30'
                     sType = "Jeu " + str(config.looking_game) + " 40-30, Joueur " + str(first_player)
                 elif config.scriptType == '4015':
-                    config.win_type = '15:40'  # inversé
+                    config.win_type = '40:15'  # inversé
                     win_texte = '40-15'
                     sType = "Jeu " + str(config.looking_game) + " 40-15, Joueur " + str(first_player)
                 elif config.scriptType == '400':
-                    config.win_type = '0:40'  # inversé
+                    config.win_type = '40:0'  # inversé
                     win_texte = '40-0'
                     sType = "Jeu " + str(config.looking_game) + " 40-0, Joueur " + str(first_player)
 
             else:
                 first_player = 2
                 if config.scriptType == '4030':
-                    config.win_type = '40:30'  # inversé
+                    config.win_type = '30:40'  # inversé
                     win_texte = '30-40'
                     sType = "Jeu " + str(config.looking_game) + " 30-40, Joueur " + str(first_player)
                 elif config.scriptType == '4015':
-                    config.win_type = '40:15'  # inversé
+                    config.win_type = '15:40'  # inversé
                     win_texte = '15-40'
                     sType = "Jeu " + str(config.looking_game) + " 15-40, Joueur " + str(first_player)
                 elif config.scriptType == '400':
                     win_texte = '0-40'
-                    config.win_type = '40:0'  # inversé
+                    config.win_type = '0:40'  # inversé
                     sType = "Jeu " + str(config.looking_game) + " 0-40, Joueur " + str(first_player)
         if config.scriptType == '030':
 
