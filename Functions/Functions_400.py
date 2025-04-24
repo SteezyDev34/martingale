3239a5174feffe0d75c9092521967fddb1062866
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-04-24 11:28:45 +0200

    improve looking game

--- Functions/Functions_400.py
+++ Functions/Functions_400.py
@@ -223,7 +223,7 @@ def all_script(driver):
             if float(config.perte) == 0 and float(config.global_match_win) < 1:
                 print('pas de perte continue')
                 continue
-            elif config.nb_tour == winmatch:
+            elif config.nb_tour < winmatch:
                 print('fin de match')
                 break
     if config.perte > 0.2:
