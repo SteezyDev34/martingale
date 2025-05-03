9a251b358402216d195619197808cbafc16c568c
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-03 10:45:01 +0200

    time clear log

--- Functions/Functions_030.py
+++ Functions/Functions_030.py
@@ -193,7 +193,7 @@ def all_script(driver):
                     config.perte = float(infosperte['perte'])
             config.rattrape_perte = 1
             config.log(f'Net profit: {config.global_match_win}')
-            if float(config.global_match_win) >= 1:
+            if float(config.global_match_win) >= 0:
                 break
     if config.perte > 0.2:
         DispatchPerte()
