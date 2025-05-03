d8a75cc89c9ed87def0cd6ae5412a8b8d2a048cb
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-03 12:36:30 +0200

    time clear log

--- SCRIPTS 40A/40A-1.py
+++ SCRIPTS 40A/40A-1.py
@@ -56,7 +56,7 @@ else:
 
 # Chargement des functions
 # Chargement de Chrome driver
-from ChromeDriver.SetDriver1 import driver
+from ChromeDriver.SetDriver import driver
 
 from Functions import Functions_40a_proba
 from Functions.GetJsonData import DispatchPerte
