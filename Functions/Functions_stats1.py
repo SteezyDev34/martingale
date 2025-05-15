ef492ac966c9c4d5128d615dc5d30d36447a2109
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-15 12:55:50 +0200

    classement

--- Functions/Functions_stats1.py
+++ Functions/Functions_stats1.py
@@ -131,6 +131,7 @@ def get_wta_proba_40A_other(playerName1, playerName2, driver, link=False):
     while ok == 0 and tentative < 4:
         i = 1
         try:
+            print(playersName)
             for playerName in playersName:
                 print(playerName)
                 driver.get('https://www.wtatennis.com/rankings/singles')
@@ -139,23 +140,24 @@ def get_wta_proba_40A_other(playerName1, playerName2, driver, link=False):
                         (By.CLASS_NAME, 'rankings__list'))
                 )
 
-                fieldplayer1 = driver.find_elements(By.CLASS_NAME, 'player-search')[0]
+                fieldplayer1 = driver.find_elements(By.CLASS_NAME, 'js-player-search-button')[0]
                 fieldplayer1.click()
                 time.sleep(2)
-                fieldplayer1 = driver.find_elements(By.CLASS_NAME, 'player-search__input')[0]
+                fieldplayer1 = driver.find_elements(By.CLASS_NAME, 'js-player-search-input')[0]
                 fieldplayer1.send_keys(playerName)
                 element = WebDriverWait(driver, 10).until(
                     EC.visibility_of_element_located(
-                        (By.CLASS_NAME, 'rankings__row'))
+                        (By.CLASS_NAME, 'js-rankings-body'))
                 )
                 time.sleep(5)
                 player_block = driver.find_elements(By.CLASS_NAME, 'js-rankings-body')[0]
-                player_list = player_block.find_elements(By.CLASS_NAME, 'rankings__row')
+                player_list = player_block.find_elements(By.CLASS_NAME, 'player-row')
                 print('lplayer list le,')
                 print(len(player_list))
                 for player in player_list:
                     print('eaach player')
-                    player_name = player.find_elements(By.CLASS_NAME, 'player-name')[0].text.strip().replace('\n', ' ')
+                    player_name = player.find_elements(By.CLASS_NAME, 'player-cell__name-wrap')[0].text.strip().replace(
+                        '\n', ' ')
                     print(player_name.lower())
                     if re.search(playerName.lower(), player_name.lower()):
                         print('find')
@@ -163,11 +165,9 @@ def get_wta_proba_40A_other(playerName1, playerName2, driver, link=False):
                     else:
                         print('not found')
                     try:
-                        statTab = WebDriverWait(driver, 10).until(
-                            EC.presence_of_element_located((By.CSS_SELECTOR, "ul.tabs__list"))
-                        )
-                        stats_tab = driver.find_element(By.CSS_SELECTOR, "li.js-tab-item[data-tab='#stats']")
-                        stats_tab.click()
+                        time.sleep(2)
+                        curretn = driver.current_url + '/stats'
+                        driver.get(curretn)
                         # Attendre que le conteneur des stats soit chargé
                         WebDriverWait(driver, 10).until(
                             EC.presence_of_element_located((By.CLASS_NAME, "player-stats__secondary-stats"))
@@ -188,8 +188,8 @@ def get_wta_proba_40A_other(playerName1, playerName2, driver, link=False):
                         time.sleep(3)
 
                         # Sélectionner l'option "2024"
-                        option_2024 = driver.find_element(By.XPATH, "//div[@data-value='2024']")
-                        option_2024.click()
+                        option_2025 = driver.find_element(By.XPATH, "//div[@data-value='2025']")
+                        option_2025.click()
                         time.sleep(3)
                         # Localiser les stats
                         stats = driver.find_elements(By.CLASS_NAME, "player-stats__secondary")
@@ -236,22 +236,25 @@ def get_wta_proba_40A_other(playerName1, playerName2, driver, link=False):
                                         prob_retour_joueur1 = float(prob_retour_joueur1) / 100
                                         print('prob_retour_joueur1 : ' + str(prob_retour_joueur1))
                         if i == 2:
+                            print('break')
                             break
                         else:
                             i = i + 1
+                        print('end')
                     except:
                         print('no stats found')
-                # Calcul de la probabilité pour le joueur et l'adversaire
-                prob_40_40_joueur1 = prob_service_joueur1 * prob_retour_joueur1
-                prob_40_40_joueur2 = prob_service_joueur2 * prob_retour_joueur2
 
-                # Calcul de la probabilité totale
-                prob_40_40_totale = prob_40_40_joueur1 + prob_40_40_joueur2
-                prob = prob_40_40_totale
-                print('Proba 40 A = ' + str(prob))
-                return prob
+            # Calcul de la probabilité pour le joueur et l'adversaire
+            prob_40_40_joueur1 = prob_service_joueur1 * prob_retour_joueur1
+            prob_40_40_joueur2 = prob_service_joueur2 * prob_retour_joueur2
+
+            # Calcul de la probabilité totale
+            prob_40_40_totale = prob_40_40_joueur1 + prob_40_40_joueur2
+            prob = prob_40_40_totale
+            print('Proba 40 A = ' + str(prob))
+            return prob
         except:
-            tentative = tentative+1
+            tentative = tentative + 1
             continue
     if link:
         driver.get(link)
@@ -437,4 +440,4 @@ def get_wta_proba_40A(playerName1, playerName2):
 if __name__ == '__main__':
     from ChromeDriver.SetDriver1 import driver
 
-    print(get_wta_proba_40A_other('Julieta Pareja', 'Leolia Jeanjean', driver))
+    print(get_wta_proba_40A_other('aryna sabalenka', 'Leolia Jeanjean', driver))
--- Functions/ScriptRechercheDeMatch.py
+++ Functions/ScriptRechercheDeMatch.py
@@ -261,21 +261,56 @@ def classementeDeMatch(driver):
             driver.get('https://ca.1xbet.com/fr/line/tennis')
             return False
         # RECUPERATION DES LIGUES EN COURS
-        bet_list_ligue = driver.find_elements(By.CLASS_NAME,
-                                              'dashboard-champ')
+        tennis_menu = driver.find_elements(By.CLASS_NAME,
+                                           'sports-menu-app-sport')
+
+        for menu in tennis_menu:
+            sport_link = menu.find_element(By.CLASS_NAME, 'sports-menu-app-sport__link')
+            sport = sport_link.find_element(By.CLASS_NAME, 'ui-nav-link-caption__label').text
+            if 'tennis de table' in sport.lower():
+                continue
+            elif 'tennis' not in sport.lower():
+                continue
+            else:
+                print('tennis trouvé')
+                print('click ok')
+                break
+        country = driver.find_element(By.CLASS_NAME, 'sports-menu-group-by-country')
+        print('find countries')
+        countrybutton = country.find_elements(By.CLASS_NAME, 'sports-menu-group-by-champ')
+        for cntrybtn in countrybutton:
+            print('contry')
+            cntrybtn.click()
+            print('click cntry')
+            if 'itf' in cntrybtn.text.lower():
+                break
+
+        liguebtn = driver.find_elements(By.CLASS_NAME, 'sports-menu-app-champ-with-sub-champs-group__item')
+        links = []
+        for lbtn in liguebtn:
+            link = lbtn.find_element(By.CLASS_NAME, 'ui-nav-link__content').get_attribute(
+                "href")
+            links.append(link)
+        print(links)
         matchlist = []
         liguelist = []
-        for bet_ligue in bet_list_ligue:
-            # ON RÉCUPÈRE LE NOM DE LA LIGUE
-            config.ligue_name = GetLigueName.main(bet_ligue)
-            # EN CAS D'ERREUR
-            if not config.ligue_name:
-                config.error = False
-                break
-            liguelist.append([bet_ligue.find_elements(By.CLASS_NAME,
-                                                      'ui-dashboard-champ-name__link')[
-                0].get_attribute(
-                "href"), config.ligue_name])
+        for link in links:
+            driver.get(link)
+            time.sleep(5)
+            bet_list_ligue = driver.find_elements(By.CLASS_NAME,
+                                                  'dashboard-champ')
+
+            for bet_ligue in bet_list_ligue:
+                # ON RÉCUPÈRE LE NOM DE LA LIGUE
+                config.ligue_name = GetLigueName.main(bet_ligue)
+                # EN CAS D'ERREUR
+                if not config.ligue_name:
+                    config.error = False
+                    break
+                liguelist.append([bet_ligue.find_elements(By.CLASS_NAME,
+                                                          'ui-dashboard-champ-name__link')[
+                    0].get_attribute(
+                    "href"), config.ligue_name])
 
         for link in liguelist:
             driver.get(link[0])
