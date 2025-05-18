da1a29533ac75e075a750a496c26db823a51c2af
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-18 11:49:41 +0200

    encoding UTF8

--- Functions/ScriptRechercheDeMatch.py
+++ Functions/ScriptRechercheDeMatch.py
@@ -367,7 +367,7 @@ def classementeDeMatch(driver):
         tableau_trie = sorted(goodmatch, key=lambda x: x[-1], reverse=True)
 
         # Retenir les 10 premières lignes
-        top_10 = tableau_trie[:50]
+        top_10 = tableau_trie[:20]
         for m in top_10:
             # Join array elements with pipe separator before adding to todo
             todo("add", "|".join(str(x) for x in m), config.matchlisttodo_file_name)
@@ -534,7 +534,7 @@ def newclassementeDeMatch(driver):
         tableau_trie = sorted(goodmatch, key=lambda x: x[-1], reverse=True)
 
         # Retenir les 10 premières lignes
-        top_10 = tableau_trie[:50]
+        top_10 = tableau_trie[:20]
         for m in top_10:
             # Join array elements with pipe separator before adding to todo
             todo("add", "|".join(str(x) for x in m), config.matchlisttodo_file_name)
