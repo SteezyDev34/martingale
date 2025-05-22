a166c66ec8dac58ceecaad32ff08ffef4bd3adee
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-22 13:27:57 +0200

    encoding UTF8

--- Functions/Functions_4015.py
+++ Functions/Functions_4015.py
@@ -240,7 +240,7 @@ def all_script(driver):
                     config.perte = float(infosperte['perte'])
             config.rattrape_perte = 1
             config.log(f'Net profit: {config.global_match_win}')
-            if float(config.global_match_win) >= 1:
+            if float(config.global_match_win) >= 0.2:
                 break
         if config.match_end:
             break
