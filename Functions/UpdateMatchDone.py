4abfa571aa4ebdfd3fe0708fdb96439539bbe54d
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-25 12:03:23 +0200

    encoding UTF8

--- Functions/UpdateMatchDone.py
+++ Functions/UpdateMatchDone.py
@@ -1,41 +1,49 @@
 # UpdateMatchDone
 # MISE A JOUR DES MATCHS EFFECTUÉS
+import chardet
+
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+        return result['encoding'] or 'utf-8'
+
+
 def main(action, values, matchlist_file_name):
+    file_path = matchlist_file_name + ".txt"
+    encoding = detect_encoding(file_path) if action == "del" else "utf-8"
+
     if action == "add":
         newmatch = values
-        with open(matchlist_file_name + ".txt", "a", encoding="utf-8") as f:
+        with open(file_path, "a", encoding="utf-8") as f:
             f.write("\n" + str(newmatch))
     elif action == "del":
-        with open(matchlist_file_name + ".txt", "r", encoding="utf-8") as f:
+        with open(file_path, "r", encoding=encoding) as f:
             content = f.read()
         match_list = content.replace("\n" + str(values), "")
-        with open(matchlist_file_name + ".txt", "w", encoding="utf-8") as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(match_list)
 
 
 def todo(action, values, matchlisttodo_file_name):
     file_path = matchlisttodo_file_name + ".txt"
+    encoding = detect_encoding(file_path)
 
     if action == "add":
-        # Lire le contenu existant
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(file_path, "r", encoding=encoding) as file:
             existing_matches = file.read().splitlines()
 
-        # Vérifier si l'élément existe déjà
         if str(values) in existing_matches:
             print(f"L'élément '{values}' existe déjà dans le fichier.")
         else:
-            # Ajouter le nouvel élément
             with open(file_path, "a", encoding="utf-8") as file:
                 file.write("\n" + str(values))
             print(f"L'élément '{values}' a été ajouté avec succès.")
 
     elif action == "del":
-        # Lire le contenu existant
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(file_path, "r", encoding=encoding) as file:
             existing_matches = file.read().splitlines()
 
-        # Supprimer l'élément s'il existe
         if str(values) in existing_matches:
             updated_matches = [match for match in existing_matches if match != str(values)]
             with open(file_path, "w", encoding="utf-8") as file:
