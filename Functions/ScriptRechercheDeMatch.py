27b28121a05db6d4b39f42206e5d5beccb3ed356
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-05-25 11:25:36 +0200

    encoding UTF8

--- Functions/ScriptRechercheDeMatch.py
+++ Functions/ScriptRechercheDeMatch.py
@@ -534,7 +534,7 @@ def newclassementeDeMatch(driver):
         tableau_trie = sorted(goodmatch, key=lambda x: x[-1], reverse=True)
 
         # Retenir les 10 premières lignes
-        top_10 = tableau_trie[:20]
+        top_10 = tableau_trie[:100]
         for m in top_10:
             # Join array elements with pipe separator before adding to todo
             todo("add", "|".join(str(x) for x in m), config.matchlisttodo_file_name)
