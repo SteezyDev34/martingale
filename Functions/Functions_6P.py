e65c6c8847c6db52ede9cf9277b9e594bfe3e3f6
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-06-04 20:38:50 +0200

    encoding UTF8

--- Functions/Functions_6P.py
+++ Functions/Functions_6P.py
@@ -46,11 +46,11 @@ def all_script(driver):
         print("#RECHERCHE INFOS DE MISE")
         infosperte = getGlobalPerte()
         if infosperte and config.perte == 0:
-            if float(infosperte['perte']) > 1:
-                SendGlobalPerte(config.scriptType, -1)
-                config.perte = 1
+            if float(infosperte['perte']) > 20:
+                SendGlobalPerte(config.scriptType, -20)
+                config.perte = 20
                 config.rattrape_perte = 1
-            elif float(infosperte['perte']) <= 1:
+            elif float(infosperte['perte']) <= 20:
                 config.perte = float(infosperte['perte'])
                 m = 0 - config.perte
                 SendGlobalPerte(config.scriptType, m)
@@ -231,18 +231,18 @@ def all_script(driver):
             infosperte = getGlobalPerte()
             print("PERTE : ")
             if infosperte:
-                if float(infosperte['perte']) > 1:
-                    SendGlobalPerte(config.scriptType, -1)
-                    config.perte = 1
+                if float(infosperte['perte']) > 20:
+                    SendGlobalPerte(config.scriptType, -20)
+                    config.perte = 20
                     config.rattrape_perte = 1
-                elif float(infosperte['perte']) <= 1:
+                elif float(infosperte['perte']) <= 20:
                     config.perte = float(infosperte['perte'])
                     m = 0 - config.perte
                     SendGlobalPerte(config.scriptType, m)
                     config.perte = float(infosperte['perte'])
             config.rattrape_perte = 1
             config.log(f'Net profit: {config.global_match_win}')
-            if float(config.global_match_win) >= 0.2:
+            if float(config.global_match_win) >= 2:
                 break
     if config.perte > 0.2:
         DispatchPerte()
