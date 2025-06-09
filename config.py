844e0fe597c158363bb8fbd10d7a61476187fa32
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-06-09 16:03:21 +0200

    encoding UTF8

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ system_description = systeme if systeme in SUPPORTED_SYSTEMS else f"Système inc
 projectPath = os.path.dirname(os.path.abspath(__file__))
 scriptTypeList = ['030', '300', '30A']
 scriptTypeList2 = ['4P', '5P', '6P', '40A']
-scriptTypeList3 = ['400', '4015', '4030', '40A']
+scriptTypeList3 = ['400', '4015', '4030']
 # Script configuration
 script_num = 0  # Numéro du Script
 win = 0  # Nombre de victoire
