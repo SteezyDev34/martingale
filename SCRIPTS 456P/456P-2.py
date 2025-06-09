c8d0b9bc026ccada676497629c40d6317740cc77
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-06-09 16:38:12 +0200

    encoding UTF8

--- SCRIPTS 456P/456P-2.py
+++ SCRIPTS 456P/456P-2.py
@@ -59,19 +59,20 @@ from ChromeDriver.SetDriver import driver
 
 from Functions import Functions_456P
 from Functions.GetJsonData import DispatchPerte
-from Functions.ScriptRechercheDeMatch import classementeDeMatch
+from Functions.ScriptRechercheDeMatch import classementeDeMatch, newclassementeDeMatch
 from Functions.Authenticator import is_logged_in, loginProcess
 
-config.scriptTypeList = config.scriptTypeList2
-config.winmatch = {script_type: 0 for script_type in config.scriptTypeList}
-config.global_match_win = {script_type: 0 for script_type in config.scriptTypeList}
-for i in config.scriptTypeList:
-    config.ScriptConfig(i)
 confirmation = input(f"Classement ? (Y/N): ")
 
 if confirmation.upper() == 'Y' or confirmation.upper() == 'y' or confirmation.upper() == 'O' or confirmation.upper() == 'o':
-    config.switchScript(config.scriptTypeList[0])
-    classementeDeMatch(driver)
+    confirmation = input(f"type de Classement ? (1/2): ")
+    if confirmation == '1':
+        classementeDeMatch(driver)
+    else:
+        print('new class')
+        newclassementeDeMatch(driver)
+        print('new class')
+        classementeDeMatch(driver)
 
 # Call the function to get the code
 if not is_logged_in(driver):
@@ -82,20 +83,33 @@ else:
 is_in = input("Voulez-vous trier les matchs ? (Y/N): ")
 if is_in.upper() == 'Y' or is_in.upper() == 'y' or is_in.upper() == 'O' or is_in.upper() == 'o':
     config.in_stat = True
+config.scriptTypeList = config.scriptTypeList2
+for i in config.scriptTypeList:
+    config.ScriptConfig(i)
+config.winmatch = {script_type: 0 for script_type in config.scriptTypeList}
+config.global_match_win = {script_type: 0 for script_type in config.scriptTypeList}
+config.running_file_name = f"{config.projectPath}/SCRIPTS 456P/running"
+config.matchlist_file_name = f"{config.projectPath}/SCRIPTS 456P/matchlist"
 while (config.win < 100):
     print(config.scriptTypeList)
+    Functions_456P.all_script(driver)
 
     try:
-        Functions_456P.all_script(driver)
+        pass
     except Exception as e:
         config.log(f"ERROR SCRIPT : {e}", 'error', False)
     else:
         if config.perte > 0:
             DispatchPerte()
         for i in config.scriptTypeList:
-            config.switchScript('456')
+            config.switchScript('456P')
             config.ScriptConfig(i).reset()
-        sucess = False
+            config.init_variable()
+            config.switchScript(i)
+            DispatchPerte()
+            config.global_match_win[i] = 0  # Initialize win counter for script type
+            config.winmatch[i] = 0  # Initialize match counter for script type
+            sucess = False
         while not sucess:
             try:
                 driver.get(config.site_url)
