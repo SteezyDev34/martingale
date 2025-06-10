60c1acc84a29b4648d3271847e24769244239307
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-06-10 11:53:46 +0200

    update gitignore

--- Functions/Functions_456P.py
+++ Functions/Functions_456P.py
@@ -291,6 +291,14 @@ def all_script(driver):
             GetIfGameEnd(driver)
     config.switchScript('4315A')
     print("update : " + config.newmatch)
+    for i in config.scriptTypeList:
+        config.switchScript('456P')
+        config.ScriptConfig(i).reset()
+        config.init_variable()
+        config.switchScript(i)
+        DispatchPerte()
+        config.global_match_win[i] = 0  # Initialize win counter for script type
+        config.winmatch[i] = 0  # Initialize match counter for script type
     Functions_1XBET.update_match_done("del", config.newmatch, config.matchlist_file_name)
     Functions_1XBET.del_running(config.script_num, config.running_file_name)
     DeleteBet(driver)
