dd4786982ab2d39e701b500c5a2604c5ceb8bb22
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-06-12 16:34:58 +0200

    update gitignore

--- SCRIPTS 456P/456P-2.py
+++ SCRIPTS 456P/456P-2.py
@@ -76,7 +76,6 @@ if confirmation.upper() == 'Y' or confirmation.upper() == 'y' or confirmation.up
 
 # Call the function to get the code
 if not is_logged_in(driver):
-    print('not logged in ')
     loginProcess(driver)
 else:
     print("You are already logged in.")
@@ -91,9 +90,11 @@ config.winmatch = {script_type: 0 for script_type in config.scriptTypeList}
 config.global_match_win = {script_type: 0 for script_type in config.scriptTypeList}
 config.running_file_name = f"{config.projectPath}/SCRIPTS 456P/running"
 config.matchlist_file_name = f"{config.projectPath}/SCRIPTS 456P/matchlist"
+config.total_want_win = config.total_want_win2
 while (config.win < 100):
+    Functions_456P.all_script(driver)
     try:
-        Functions_456P.all_script(driver)
+        print('e')
     except Exception as e:
         config.log(f"ERROR SCRIPT : {e}", 'error', False)
     else:
