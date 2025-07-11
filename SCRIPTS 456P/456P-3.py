14a7117d5014cc1d70e65d4a54bc3e0733decf8e
Auxobetting <155380294+Auxobetting@users.noreply.github.com>
2025-07-11 03:19:23 +0200

    New site add windows sys parameter

--- SCRIPTS 456P/456P-3.py
+++ SCRIPTS 456P/456P-3.py
@@ -91,12 +91,10 @@ config.winmatch = {script_type: 0 for script_type in config.scriptTypeList}
 config.global_match_win = {script_type: 0 for script_type in config.scriptTypeList}
 config.running_file_name = f"{config.projectPath}/SCRIPTS 456P/running"
 config.matchlist_file_name = f"{config.projectPath}/SCRIPTS 456P/matchlist"
+config.total_want_win = config.total_want_win2
 while (config.win < 100):
-    print(config.scriptTypeList)
-    Functions_456P.all_script(driver)
-
     try:
-        pass
+        Functions_456P.all_script(driver)
     except Exception as e:
         config.log(f"ERROR SCRIPT : {e}", 'error', False)
     else:
