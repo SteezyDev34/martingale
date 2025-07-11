a81017e1a4467310d54a361ac8502aa207604b39
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-07-11 16:26:01 +0200

    improve script

--- SCRIPTS 456P/456P-3.py
+++ SCRIPTS 456P/456P-3.py
@@ -76,7 +76,6 @@ if confirmation.upper() == 'Y' or confirmation.upper() == 'y' or confirmation.up
 
 # Call the function to get the code
 if not is_logged_in(driver):
-    print('not logged in ')
     loginProcess(driver)
 else:
     print("You are already logged in.")
@@ -91,12 +90,10 @@ config.winmatch = {script_type: 0 for script_type in config.scriptTypeList}
 config.global_match_win = {script_type: 0 for script_type in config.scriptTypeList}
 config.running_file_name = f"{config.projectPath}/SCRIPTS 456P/running"
 config.matchlist_file_name = f"{config.projectPath}/SCRIPTS 456P/matchlist"
+config.total_want_win = config.total_want_win
 while (config.win < 100):
-    print(config.scriptTypeList)
-    Functions_456P.all_script(driver)
-
     try:
-        pass
+        Functions_456P.all_script(driver)
     except Exception as e:
         config.log(f"ERROR SCRIPT : {e}", 'error', False)
     else:
--- config.py
+++ config.py
@@ -117,9 +117,8 @@ teams = False
 winmatch = {script_type: 0 for script_type in scriptTypeList}
 global_match_win = {script_type: 0 for script_type in scriptTypeList}
 
-total_want_win = {'030': 2, '300': 2, '15A': 2, '30A': 2, 'BREAK': 0.2}
-total_want_win2 = {'4P': 2, '5P': 2, '6P': 2, '40A': 2}
-total_want_win3 = {'400': 2, '4015': 2, '4030': 2}
+total_want_win = {'030': 2, '300': 2, '15A': 2, '30A': 2, 'BREAK': 0.2, '400': 2, '4015': 2, '4030': 2, '4P': 2,
+                  '5P': 2, '6P': 2, '40A': 2}
 
 
 def getJsonData(url: str) -> Optional[Dict[str, Any]]:
