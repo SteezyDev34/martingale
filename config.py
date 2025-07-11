e7dc7fe85e86026b41ef0e9ef38414fd92caf913
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-07-11 18:35:06 +0200

    improve script

--- config.py
+++ config.py
@@ -16,8 +16,9 @@ system_description = systeme if systeme in SUPPORTED_SYSTEMS else f"Système inc
 # Project path initialization
 projectPath = os.path.dirname(os.path.abspath(__file__))
 scriptTypeList = ['030', '300', '15A', '30A']
-scriptTypeList2 = ['4P', '5P', '6P', '40A']
-scriptTypeList3 = ['400', '4015', '4030' 'BREAK']
+scriptTypeList2 = ['4P', '6P', ]
+scriptTypeList3 = ['5P', '40A']
+scriptTypeList4 = ['400', '4015', '4030' 'BREAK']
 
 # Script configuration
 script_num = 0  # Numéro du Script
