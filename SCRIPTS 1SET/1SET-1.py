fd7f1487fa100ff535dec2a9bd2aa35b0783497e
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-07-24 04:39:47 +0200

    improve script

--- SCRIPTS 1SET/1SET-1.py
+++ SCRIPTS 1SET/1SET-1.py
@@ -86,10 +86,9 @@ for i in config.scriptTypeList:
     config.ScriptConfig(i)
 
 while (config.win < 100):
-    Functions_1SET.all_script(driver)
 
     try:
-        pass
+        Functions_1SET.all_script(driver)
     except Exception as e:
         config.log(f"ERROR SCRIPT : {e}", 'error', False)
     else:
