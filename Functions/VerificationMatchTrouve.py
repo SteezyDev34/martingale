0a4d5f62739c1018858d2b2819011328f16c03d9
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-08-19 19:08:32 +0200

    add break and test

--- Functions/VerificationMatchTrouve.py
+++ Functions/VerificationMatchTrouve.py
@@ -29,6 +29,7 @@ def main(driver, bet_item, matchlist_file_name):
         print(match_list)
         if config.scriptType != '1SET':
             config.log('TOUT match autorisé!', 'success', False, 4)
+            driver.get(newmatchtxt)
             return [True, config.newmatch]
         elif config.in_stat and any(config.newmatch in x for x in match_list) and not any(
                 config.newmatch in x for x in match_done):
