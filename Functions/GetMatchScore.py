e8665794282d525be56958c73d027ba29d19aedd
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-09-14 10:40:58 +0200

    rsync file

--- Functions/GetMatchScore.py
+++ Functions/GetMatchScore.py
@@ -7,7 +7,7 @@ import config
 def main(div_bet_score: object, score_to_start: list[str]) -> bool:
     get_bet_score = False
     try:
-        config.log('Récupération du score', 'info', True, 4)
+        # config.log('Récupération du score', 'info', True, 4)
         config.log_clear_line()
         get_if_icon_ball = div_bet_score.find_elements(By.CLASS_NAME,
                                                        config.classes[
@@ -24,13 +24,13 @@ def main(div_bet_score: object, score_to_start: list[str]) -> bool:
         config.log('Impossible de lire le score du match!', 'warning', False, 3)
         get_bet_score = False
     else:
-        config.log('Score en cours : ' + bet_score, 'warning', True, 4)
+        # config.log('Score en cours : ' + bet_score, 'warning', True, 4)
         config.log_clear_line()
         if any(score_ok == bet_score
                for score_ok in
                score_to_start) and len(get_if_icon_ball) > 0:
             get_bet_score = True
-            config.log('Score eOKOK : ' + bet_score, 'warning', True, 4)
+            # config.log('Score eOKOK : ' + bet_score, 'warning', True, 4)
             config.log_clear_line()
     return get_bet_score
 
@@ -38,7 +38,7 @@ def main(div_bet_score: object, score_to_start: list[str]) -> bool:
 def set1main(div_bet_score: object, score_to_start: list[str]) -> bool:
     get_bet_score = False
     try:
-        config.log('Récupération du score', 'info', True, 4)
+        # config.log('Récupération du score', 'info', True, 4)
         config.log_clear_line()
 
         bet_score = div_bet_score.text
@@ -48,12 +48,12 @@ def set1main(div_bet_score: object, score_to_start: list[str]) -> bool:
         config.log('Impossible de lire le score du match!', 'warning', False, 3)
         get_bet_score = False
     else:
-        config.log('Score en cours : ' + bet_score, 'warning', True, 4)
+        # config.log('Score en cours : ' + bet_score, 'warning', True, 4)
         config.log_clear_line()
         if any(score_ok == bet_score
                for score_ok in
                score_to_start):
             get_bet_score = True
-            config.log('Score eOKOK : ' + bet_score, 'warning', True, 4)
+            # config.log('Score eOKOK : ' + bet_score, 'warning', True, 4)
             config.log_clear_line()
     return get_bet_score
