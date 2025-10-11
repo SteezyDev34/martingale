e36c30f00c06cbf21a90e83689227ae27c0f181d
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-10-12 01:26:39 +0200

    config

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ system_description = systeme if systeme in SUPPORTED_SYSTEMS else f"Système inc
 
 # Project path initialization
 projectPath = os.path.dirname(os.path.abspath(__file__))
-scriptTypeList = ['030','300', '15A', '30A']
-scriptTypeList1 = ['4P,'400','40A']
+scriptTypeList = ['030', '300', '15A', '30A']
+scriptTypeList1 = ['4P,' '400', '40A']
 scriptTypeList2 = ['5P', '4015', 'BREAK']
 scriptTypeList3 = ['6P', '4030']
 scriptTypeList4 = ['BREAK', '4015', '4030']
