23db9b2ed8d477e0d20f5550652ef691f2485133
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-10-24 19:34:42 +0200

    fix: Remove trailing whitespace in start_all.ps1

--- start_all.ps1
+++ start_all.ps1
@@ -4,7 +4,7 @@ $Tabs = @(
     @{Title = "1530A-1"; Script = "$Project\1530A-1.ps1"},
     @{Title = "5P40A-1"; Script = "$Project\5P40A-1.ps1"},
     @{Title = "4P6P-1"; Script = "$Project\4P6P-1.ps1"},
-    @{Title = "BREAK-1"; Script = "$Project\BREAK-1.ps1"},
+    @{Title = "BREAK-1"; Script = "$Project\BREAK-1.ps1"}
 )
 
 # --- Créer dossier logs s'il n'existe pas ---
