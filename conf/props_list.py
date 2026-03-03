c7302e5bd26771e042c8f25c0f6b516230f67d01
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2026-03-03 22:01:45 +0100

    wip

---
+++ conf/props_list.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Liste minimale des "player props" NBA utilisée par le nouvel extracteur
+# Ajout : versions EN et FR
+nba_props_list = {
+    "Player Props": [
+        "Points",
+        "Rebounds",
+        "Assists",
+        "Points+Rebounds+Assists",
+        "3PM",
+        "Blocks",
+        "Steals"
+    ],
+    "Player Props (FR)": [
+        "Points",
+        "Rebonds",
+        "Passes décisives",
+        "Points+Rebonds+Passes décisives",
+        "Tirs à 3 points réussis",
+        "Contres",
+        "Interceptions"
+    ]
+}
