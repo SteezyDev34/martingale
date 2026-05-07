ce41289398288bc73f0c52eda20c3b7e0098f196
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2026-05-07 17:40:32 +0200

    Ajout d'un break dans la fonction classementeDeMatch pour interrompre la boucle après avoir trouvé le premier lien valide.

--- Functions/ScriptRechercheDeMatch.py
+++ Functions/ScriptRechercheDeMatch.py
@@ -661,6 +661,7 @@ def classementeDeMatch(driver, use_json_cache=True):
                                                           config.classes['dashboard_champ_name'][config.site_type])[
                     0].get_attribute(
                     "href"), config.ligue_name])
+                break
 
             for link in liguelist:
                 if link[0]:
