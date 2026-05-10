308a56033f266abaed1c94b01bb169ec03d4d4b9
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2026-05-10 15:39:15 +0200

    Amélioration de la condition de vérification des profits nets pour exclure les scripts en cours d'exécution

--- Functions/Functions_15V1.py
+++ Functions/Functions_15V1.py
@@ -361,7 +361,7 @@ def all_script(driver):
             all_below_one = all(
                 float(config.global_match_win[st]) >= float(config.total_want_win[st]) for st in
                 config.scriptTypeList)
-            if all_below_one:
+            if all_below_one and not RedisIPC.list_running(exclude_script_type=config.scriptType, matchname=config.newmatch):
                 for st in config.scriptTypeList:
                     config.log(f' {st} Net profit: {config.global_match_win[st]} / {config.total_want_win[st]}',
                                'success', False)
